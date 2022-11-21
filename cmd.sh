b8ac35b816d20698d7bfa8eaca2eb5aceb61432d
hungnguyendinh <hungnguyendinh.501@gmail.com>
2022-11-21 08:33:20 +0700

    add dbt commands

--- cmd.sh
+++ cmd.sh
@@ -1,9 +1,14 @@
-dbt debug --profiles-dir ~/Projects/dbt-clickhouse/ --target dev
+dbt debug --profiles-dir ../ --target dev
 
-dbt test --profiles-dir ~/Projects/dbt-clickhouse/ --target dev
+dbt test --profiles-dir ../ --target dev
 
-dbt build --profiles-dir ~/Projects/dbt-clickhouse/ --target dev
+dbt build --profiles-dir ../ --target dev
 
-dbt run --profiles-dir ~/Projects/dbt-clickhouse/ --target dev
+dbt run --profiles-dir ../ --target dev
+dbt run --profiles-dir ../ --target dev --vars '{"date": "2022-11-17"}'
+dbt run --profiles-dir ../ --target dev --vars '{"date": "2022-11-17"}' --full-refresh
+dbt run --profiles-dir ../ --target dev --vars '{"date": "2022-11-17"}' --full-refresh --select website_analysis
+dbt run --profiles-dir ../ --target dev --full-refresh --select tag:daily
 
-dbt run --profiles-dir ~/Projects/dbt-clickhouse/ --target dev --vars '{"date": "2022-11-17"}'
+dbt docs generate --profiles-dir ../ --target dev
+dbt docs serve --profiles-dir ../ --target dev
